0398f2be23af4213a6f374ab248b87c88cb8980f
Tessa van de Langenberg <bliksem@wmmt.nl>
2023-11-09 20:39:11 +0100

    navbar root

--- nav.js
+++ nav.js
@@ -3,31 +3,31 @@ var navbar = ` <div class="navbar">
 
                 <div class="dropdown">
                     <button class="dropbtn">
-                        <a href="learningoutcomes.html">Learning outcomes</a>
+                        <a href="/learningoutcomes.html">Learning outcomes</a>
                         <i class="fa fa-caret-down"></i>
                     </button>
                 </div>
 
                 <div class="dropdown">
                     <button class="dropbtn">
-                        <a href="products/products.html">Products ⮟</a>
+                        <a href="/products/products.html">Products ⮟</a>
                         <i class="fa fa-caret-down"></i>
                     </button>
                     <div class="dropdown-content">
-                        <a href="products/product1.html">Product temp 1</a>
-                        <a href="products/product2.html">Product temp 2</a>
+                        <a href="/products/product1.html">Product temp 1</a>
+                        <a href="/products/product2.html">Product temp 2</a>
                     </div>
                 </div>
 
                 <div class="dropdown">
                     <button class="dropbtn">
-                        <a href="readingguide.html">Reading Guide</a>
+                        <a href="/readingguide.html">Reading Guide</a>
                         <i class="fa fa-caret-down"></i>
                     </button>
                 </div>
                 <div class="dropdown">
                     <button class="dropbtn">
-                        <a href="index.html"><b>Home</b></a>
+                        <a href="/index.html"><b>Home</b></a>
                         <i class="fa fa-caret-down"></i>
                     </button>
                 </div>
