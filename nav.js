57990f0244ea95214a8259080b813b7a962abc09
Tessa van de Langenberg <bliksem@wmmt.nl>
2024-01-18 16:13:36 +0100

    linking html and adding new pages

--- nav.js
+++ nav.js
@@ -9,17 +9,45 @@ var navbar = ` <div class="navbar">
                 </div>
 
                 <div class="dropdown">
+                <button class="dropbtn">
+                    <a href="/products/portfolio.html">Portfolio ⮟</a>
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="/products/projectplan_port.html">Project plan</a>
+                    <a href="/products/research_port.html">Research other portfolios</a>
+                    <a href="/products/libraries.html">Research libraries</a>
+                    <a href="/products/design_port.html">Design document portfolio</a>
+                    <a href="/products/c4_port.html">C4 model portfolio</a>
+                    <a href="/products/international.html">International week</a>
+                </div>
+            </div>
+
+                <div class="dropdown">
                     <button class="dropbtn">
-                        <a href="/products/products.html">Products ⮟</a>
+                        <a href="/products/products.html">Samen aan Z ⮟</a>
                         <i class="fa fa-caret-down"></i>
                     </button>
                     <div class="dropdown-content">
                         <a href="/products/projectplan.html">Project plan</a>
+                        <a href="/products/group_agreement.html">Group agreement</a>
                         <a href="/products/persona.html">Personas and user journeys</a>
                         <a href="/products/ideation.html">Ideation and storyboard</a>
-
+                        <a href="/products/qr.html">QR code test</a>
+                        <a href="/products/tine.html">Tine expert interview</a>
+                        <a href="/products/mark.html">Mark Klerkx meeting</a>
+                        <a href="/products/paper.html">Ideation session and paper prototype</a>
+                        <a href="/products/paper_test.html">Paper prototype test</a>
+                        <a href="/products/c4_app.html">C4 model application</a>
+                        <a href="/products/survey.html">SurveyJS research</a>
+                        <a href="/products/design_app.html">Design document application</a>
+                        <a href="/products/test_app.html">Test application</a>
+                        <a href="/products/advice.html">Advice document</a>
+                        <a href="/products/communication.html">Communication document</a>
                     </div>
                 </div>
+
+
 
                 <div class="dropdown">
                     <button class="dropbtn">
